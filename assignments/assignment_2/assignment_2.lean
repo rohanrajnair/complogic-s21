e9c9bf15bf3bcbe033c02923558720f2825dcd19
Rohan Nair <rnair246@gmail.com>
2021-02-16 16:00:48 -0800

    hw 2.4

--- assignments/assignment_2/assignment_2.lean
+++ assignments/assignment_2/assignment_2.lean
@@ -225,6 +225,14 @@ structure box (α : Type u) : Type u :=
 
 -- YOUR FUNCTION HERE
 
+def liftF2Box : Π {α : Type u} {β : Type u}, (α → β) → (box α) → (box β) := 
+fun a,
+    fun b,
+        fun f,
+            fun n,
+                box.mk (f n.val)
+
+
 
 -- WHEN YOU'VE GOT IT, THIS TEST SHOULD PASS
 
